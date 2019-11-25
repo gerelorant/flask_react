fc93b887fd8d314a4e9aa5f53bdeda1c8b5b0dbe
Gere Lóránt <gerelorant@gmail.com>
2019-11-25 11:17:08 +0100

    Locally serving CDN files.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='Flask-ReactJS',
-    version='0.2.2',
+    version='0.2.3',
     packages=['flask_react'],
     url='https://github.com/gerelorant/flask_react',
     license='MIT',
